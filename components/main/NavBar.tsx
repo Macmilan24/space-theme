0285aaea33e5b5378c8e37ae5594fa6cdc50fb77
Macmilan24 <cpt.macmilan24@gmail.com>
2024-11-05 15:48:23 +0300

    Update: NavLinks

--- components/main/NavBar.tsx
+++ components/main/NavBar.tsx
@@ -6,23 +6,20 @@ const NavBar = () => {
   return (
     <div className="w-full h-[65px] fixed top-0 shadow-lg  bg-[#03001417] z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px">
-        <a
-          href="#about-me"
-          className="h-auto w-auto flex flex-row items-center"
-        >
+        <a href="/" className="h-auto w-auto flex flex-row items-center">
           <span className="font-bold ml-[10px] hidden md:block text-gray-300">
-            Samuel
+            Samuel Dagne
           </span>
         </a>
         <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
           <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200 ">
-            <a href="#about-me" className="cursor-pointer">
+            <a href="/#about-me" className="cursor-pointer">
               About me
             </a>
-            <a href="#skills" className="cursor-pointer">
+            <a href="/#skills" className="cursor-pointer">
               Skills
             </a>
-            <a href="#projects" className="cursor-pointer">
+            <a href="/#projects" className="cursor-pointer">
               Projects
             </a>
           </div>
